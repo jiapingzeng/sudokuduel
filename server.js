de0a8ca3308826f05ffd4e20b0907f9457d8b1ec
Jiaping Zeng <jiaping@gisar.me>
2017-10-05 22:30:07 -0700

    included build

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express')
 const http = require('http')
 const socketIo = require('socket.io')
+const path = require('path')
 
 const port = process.env.PORT || 3001
 const app = express()
 const server = http.createServer(app)
 const io = socketIo(server)
 
+app.use(express.static(path.resolve(__dirname, 'client', 'build')))
+
 app.get('/', (req, res, next) => {
-    res.sendStatus(200)
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
 })
 
 io.on('connection', socket => {
